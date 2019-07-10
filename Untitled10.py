0a2de4c91c7fe72140dfd40946427f465db74cf3
adharshreddy <52529806+adharshreddy@users.noreply.github.com>
2019-07-10 15:36:42 +0530

    Add files via upload

---
+++ Untitled10.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+s2 = "python programming"
+s3 = "python programming"
+print(s2.istitle())
+print(s3.istitle())
+
+
+# In[2]:
+
+
+s2 = "application1889"
+s3 = 'pythonprogramming'
+print(s2.isalpha())
+print(s3.isalpha())
+
+
+# In[ ]:
+
+
+
+
